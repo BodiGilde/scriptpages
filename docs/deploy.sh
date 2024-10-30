900a93af573314c68dda1da19d71bb7b8f169d30
BodiGilde <Bodi.pubben@Student.GildeOpleidingen.nl>
2024-10-30 11:34:22 +0100

    sudo aangepast om invoer te ondersteunen

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Maak een tijdelijke RAM-disk aan met tmpfs
-ramdisk=$(mktemp -d)
-mount -t tmpfs -o size=1M tmpfs $ramdisk
-
 # Vraag om de GitHub repository URL
 read -p "Voer de GitHub repository URL in (bijv. gebruiker/repository): " repo_url
 
@@ -15,7 +11,7 @@ select auth_method in "Token" "Wachtwoord"; do
             read -s -p "Voer uw GitHub persoonlijke toegangstoken in: " token
             echo
             # Schrijf de credentials naar een bestand in de RAM-disk
-            echo "machine github.com login $token password x-oauth-basic" > $ramdisk/.netrc
+            echo "machine github.com login $token password x-oauth-basic" > /tmp/.netrc
             break
             ;;
         "Wachtwoord")
@@ -23,7 +19,7 @@ select auth_method in "Token" "Wachtwoord"; do
             read -s -p "Voer uw GitHub wachtwoord in: " password
             echo
             # Schrijf de credentials naar een bestand in de RAM-disk
-            echo "machine github.com login $username password $password" > $ramdisk/.netrc
+            echo "machine github.com login $username password $password" > /tmp/.netrc
             break
             ;;
         *)
@@ -33,13 +29,12 @@ select auth_method in "Token" "Wachtwoord"; do
 done
 
 # Stel de juiste permissies in voor het .netrc bestand
-chmod 600 $ramdisk/.netrc
+chmod 600 /tmp/.netrc
 
 # Clone de repository lokaal met gebruik van de credentials in de RAM-disk
-GIT_CURL_VERBOSE=1 GIT_TERMINAL_PROMPT=0 GIT_ASKPASS=true GIT_CONFIG_NOSYSTEM=1 GIT_SSL_NO_VERIFY=true git -c http.extraheader="AUTHORIZATION: basic $(echo -n $username:$password | base64)" clone https://github.com/$repo_url
+sudo git clone https://github.com/$repo_url
 
-# Ontkoppel en verwijder de RAM-disk na gebruik
-umount $ramdisk
-rm -rf $ramdisk
+# Verwijder het .netrc bestand na gebruik
+rm /tmp/.netrc
 
 echo "Repository succesvol gecloned."
